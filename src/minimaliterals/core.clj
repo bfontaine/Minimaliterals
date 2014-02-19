34a8c58e27819a15ae79f5f1d9e2f389eefebc18
Baptiste Fontaine <batifon@yahoo.fr>
2014-02-19 23:40:14 +0100

    %q - string litteral

--- src/minimaliterals/core.clj
+++ src/minimaliterals/core.clj
@@ -1,6 +1,12 @@
-(ns minimaliterals.core)
+(ns minimaliterals.core
+  (:require [clojure.string :as cs]))
 
 (defmacro %w
   "strings list litteral, e.g.: (%w a b c) => '(\"a\" \"b\" \"c\")"
   [& els]
   `(map str '~els))
+
+(defmacro %q
+  "string litteral, e.g.: (%q a b c) => \"a b c\""
+  [& els]
+  `(cs/join " " '~els))
--- test/minimaliterals/core_test.clj
+++ test/minimaliterals/core_test.clj
@@ -11,3 +11,13 @@
     (is (= '("a" "b" "c") (%w a b c))))
   (testing "non-letters"
     (is (= '("1" "+" "1" "=" "2") (%w 1 + 1 = 2)))))
+
+(deftest percent-q-test
+  (testing "empty list"
+    (is (= "" (%q))))
+  (testing "one arg"
+    (is (= "foo" (%q foo))))
+  (testing "multiple args"
+    (is (= "a b c" (%q a b c))))
+  (testing "non-letters"
+    (is (= "1 + 1 = 2" (%q 1 + 1 = 2)))))
